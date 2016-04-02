bcacf49b04ae19382cba7175220cab4322d1331f
Olav Trauschke <olav.trauschke@student.uva.nl>
2016-04-02 18:13:31 +0200

    Work on forms

--- web/scripts/hideForm.js
+++ web/scripts/hideForm.js
@@ -1,4 +1,9 @@
-function hideForm(form) {
+function hideForm(event, form) {
+    event = event || window.event;
+    event.preventDefault();
+    
     form.style.display = "none";
     document.getElementById("formAlt").style.display = "initial";
+    
+    $.ajax(form.action, {method: "POST", data: $(form).serialize()});
 }
