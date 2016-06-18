a0133bf01db657839a14ce328fba743093073135
Olav Trauschke <olav.trauschke@student.uva.nl>
2016-06-18 11:01:22 +0200

    Alternatieve navigatie voor als JavaScript niet werkt of een pagina buiten het frame wordt weergegeven.

--- web/scripts/internalLinks.js
+++ web/scripts/internalLinks.js
@@ -1,19 +1,39 @@
 function goToContactgegevens() {
+    if (window.top === window.self) {
+        return true;
+    }
     window.parent.goToContactgegevens();
+    return false;
 }
 
 function goToContactformulier() {
+    if (window.top === window.self) {
+        return true;
+    }
     window.parent.goToContactformulier();
+    return false;
 }
 
 function goToInschrijfformulier() {
+    if (window.top === window.self) {
+        return true;
+    }
     window.parent.goToInschrijfformulier();
+    return false;
 }
 
 function goToVideos() {
+    if (window.top === window.self) {
+        return true;
+    }
     window.parent.goToVideos();
+    return false;
 }
 
 function goToAanmeldingClinic() {
+    if (window.top === window.self) {
+        return true;
+    }
     window.parent.goToAanmeldingClinic();
+    return false;
 }
