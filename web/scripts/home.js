cc3562066abf762cfcc075b2142399ac3fc6dd74
Olav Trauschke <olav.trauschke@student.uva.nl>
2016-08-21 17:24:40 +0200

    Informatie over introductie

--- web/scripts/home.js
+++ web/scripts/home.js
@@ -2,10 +2,10 @@ function init() {
     var photos = [].slice.call(document.getElementsByClassName("newsPhoto"));
     for (var i = 0; i < photos.length; i++) {
         var photo = photos[i];
-        var article = photo.parentElement;
+        var text = photo.parentElement.getElementsByClassName("articleContent")[0];
         var factor = Math.min(
-            ((0.49 * article.clientWidth) / photo.naturalWidth),
-            (article.clientHeight / photo.naturalHeight)
+            ((0.49 * text.clientWidth) / photo.naturalWidth),
+            (text.clientHeight / photo.naturalHeight)
         );
         var width = Math.round(factor * photo.naturalWidth);
         var height = Math.round(factor * photo.naturalHeight);
